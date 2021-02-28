1693f5a526cd0a97af976f5843b8c0e6f00f1d33
Tamim-Hossain <tamim_hossain@outlook.com>
2021-02-28 14:51:47 +0600

    pirvate route and favicon changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NoMatch from "./components/404/NoMatch";
+import PrivateRoute from "./components/auth/PrivateRoute";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
 import Booking from "./components/booking/Booking";
@@ -8,7 +9,6 @@ import Review from "./components/complete/Review";
 import Thanks from "./components/complete/Thanks";
 import Main from "./components/home/Main";
 import Hotel from "./components/hotel/Hotel";
-import PrivateRoute from "./components/private/PrivateRoute";
 import Menu from "./components/shared/Menu";
 
 export const UserContext = createContext();
@@ -23,9 +23,7 @@ const App = () => {
 
 	const [bookingInfo, setBookingInfo] = useState();
 	return (
-		<UserContext.Provider
-			value={[userInfo, setUserInfo, bookingInfo, setBookingInfo]}
-		>
+		<UserContext.Provider value={[userInfo, setUserInfo, bookingInfo, setBookingInfo]}>
 			<BrowserRouter>
 				<Menu />
 				<Switch>
